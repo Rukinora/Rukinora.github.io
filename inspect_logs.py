e45de306eb31cd61e3e1e1fbb4a3cbb5508d4bfb
Ruben Flores <cs.rubenflores@gmail.com>
2026-05-18 13:11:31 -0700

    test

---
+++ inspect_logs.py
@@ -0,0 +1,7 @@
+import zipfile
+with zipfile.ZipFile('job-76605953158-logs.zip') as z:
+    print(z.namelist())
+    for n in z.namelist():
+        print('---', n)
+        data = z.read(n).decode('utf-8', errors='replace')
+        print(data[:4000])
